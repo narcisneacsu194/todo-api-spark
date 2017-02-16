17f57709560db391e30c3d2ccf785cbb93e52a25
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-02-16 23:04:08 +0200

    Adding Todo model.

---
+++ src/main/java/com/teamtreehouse/techdegrees/model/Todo.java
@@ -0,0 +1,56 @@
+package com.teamtreehouse.techdegrees.model;
+
+public class Todo {
+    private int id;
+    private String name;
+    private boolean done;
+
+    public Todo(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Todo todo = (Todo) o;
+
+        if (id != todo.id) return false;
+        if (done != todo.done) return false;
+        return name.equals(todo.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (done ? 1 : 0);
+        return result;
+    }
+}
