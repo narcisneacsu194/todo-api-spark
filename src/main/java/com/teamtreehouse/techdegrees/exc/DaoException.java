c2d26488185e3ee034173d8abaa6ed9ae0b6a76a
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-02-16 23:04:48 +0200

    Adding DaoException class.

---
+++ src/main/java/com/teamtreehouse/techdegrees/exc/DaoException.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.techdegrees.exc;
+
+import org.sql2o.Sql2oException;
+
+
+public class DaoException extends Exception {
+    private Exception originalException;
+
+    public DaoException(Sql2oException originalException, String msg) {
+        super(msg);
+        this.originalException = originalException;
+    }
+}
