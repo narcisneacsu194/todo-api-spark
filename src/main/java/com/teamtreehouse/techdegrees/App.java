1cf26a03420d5b17991b7de4b0429b5af9c9ada8
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-02-18 16:43:42 +0200

    Added api methods in the App main method.

--- src/main/java/com/teamtreehouse/techdegrees/App.java
+++ src/main/java/com/teamtreehouse/techdegrees/App.java
@@ -1,16 +1,62 @@
 package com.teamtreehouse.techdegrees;
 
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import com.google.gson.Gson;
+import com.teamtreehouse.techdegrees.dao.Sql2oTodoDao;
+import com.teamtreehouse.techdegrees.dao.TodoDao;
+import com.teamtreehouse.techdegrees.model.Todo;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
 
 public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
+        String datasource = "jdbc:h2:~/todos.db";
+        final String API_URI = "/api/v1";
 
-        get("/blah", (req, res) -> "Hello!");
+        Sql2o sql2o = new Sql2o(
+                String.format("%s;INIT=RUNSCRIPT from 'classpath:db/init.sql'", datasource),
+                "", ""
+        );
+        TodoDao todoDao = new Sql2oTodoDao(sql2o);
+        Gson gson = new Gson();
+
+        post(API_URI + "/todos", "application/json", (req, res) ->{
+            Todo todo = gson.fromJson(req.body(), Todo.class);
+            todoDao.add(todo);
+            res.status(201);
+            return todo;
+        }, gson::toJson);
+
+        get(API_URI + "/todos", "application/json", (req, res) ->
+            todoDao.findAll(), gson::toJson);
+
+        put(API_URI + "/todos/:todoId", "application/json", (req, res) ->{
+            int todoId = Integer.parseInt(req.params("todoId"));
+            Todo todo = gson.fromJson(req.body(), Todo.class);
+            todo.setId(todoId);
+            todoDao.update(todo);
+            res.status(201);
+           return todo;
+        }, gson::toJson);
+
+        delete(API_URI + "/todos/:todoId", "application/json", (req, res) ->{
+            int todoId = Integer.parseInt(req.params("todoId"));
+            Todo todo = todoDao.findById(todoId);
+            todoDao.delete(todo);
+            res.status(201);
+            return todo;
+        }, gson::toJson);
+
+
+        after((req, res) ->{
+           res.type("application/json");
+        });
 
     }
+
+
 
 }
